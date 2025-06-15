1b4f30bb5a851d8b7dd7570727eef3ab2793886b
Mehedi Hasan <64748905+mehedihasan9339@users.noreply.github.com>
2025-06-15 20:16:10 +0600

    sfds

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+// Theme Config, check the documentation before edit this file
+var site_url = "https://ghost.estudiopatagon.com/zento";
+var theme_config = {
+  masonry: "on", // If you don't need masonry style, just set to "off"
+  sticky_sidebar: "on", // on / off
+  disqus_shortname: "ghostexample", // You can create your own Disqus Comments here: https://disqus.com/admin/create/
+};
+theme_config.disqus_shortname = "ghostexample";
